cda73392cc65699b0dbe689cbed64d257ad6a6f9
Shubhendu <shtripat@redhat.com>
2017-01-05 16:12:44 +0530

    Populate the current node's id as member in manager
    tendrl-bug-id: Tendrl/ceph_integration#72
    tendrl-spec: Tendrl/gluster_integration/pull/102
    
    Signed-off-by: Shubhendu <shtripat@redhat.com>

--- tendrl/ceph_integration/manager/manager.py
+++ tendrl/ceph_integration/manager/manager.py
@@ -79,6 +79,7 @@ class CephIntegrationManager(Manager):
         ).__init__(
             "sds",
             cluster_id,
+            utils.get_node_context(),
             config,
             CephIntegrationSyncStateThread(self, cluster_id),
             CephIntegrationEtcdPersister(config),
