790fa604141b3221a71d59b37e0c168b5ae50ba0
Rohan Kanade <r0h4n@users.noreply.github.com>
2017-01-25 12:46:02 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tendrl-ceph-integration",
-    version="1.1",
+    version="1.2",
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*",
                                     "tests"]),
     namespace_packages=['tendrl'],
