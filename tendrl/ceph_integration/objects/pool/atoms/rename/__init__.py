9c9c76b18c81cbd831fc093cf87075f856f44369
Shubhendu <shtripat@redhat.com>
2017-05-17 14:03:34 +0530

    Added flow for renaming ceph pools
    tendrl-bug-id: Tendrl/api#168
    Signed-off-by: Shubhendu <shtripat@redhat.com>

--- tendrl/ceph_integration/objects/pool/atoms/rename/__init__.py
+++ tendrl/ceph_integration/objects/pool/atoms/rename/__init__.py
@@ -8,42 +8,29 @@ from tendrl.commons.message import Message
 from tendrl.commons.objects import AtomExecutionFailedError
 
 
-class Update(objects.BaseAtom):
-    obj = Pool
+class Rename(objects.BaseAtom):
     def __init__(self, *args, **kwargs):
-        super(Update, self).__init__(*args, **kwargs)
+        super(Rename, self).__init__(*args, **kwargs)
 
     def run(self):
         pool_id = self.parameters['Pool.pool_id']
         attrs = {}
-        if 'Pool.pg_num' in self.parameters:
-            fetched_obj = Pool(
-                pool_id=self.parameters['Pool.pool_id']
-            ).load()
-            attrs['pg_num'] = self.parameters.get('Pool.pg_num')
-            if attrs['pg_num'] <= int(fetched_obj.pg_num):
-                raise AtomExecutionFailedError(
-                    "New pg-num cannot be less than existing value"
-                )
-        if 'Pool.size' in self.parameters:
-            attrs['size'] = self.parameters.get('Pool.size')
-        if 'Pool.min_size' in self.parameters:
-            attrs['min_size'] = self.parameters.get('Pool.min_size')
-        if 'Pool.quota_enabled' in self.parameters and \
-            self.parameters['Pool.quota_enabled'] is True:
-            attrs['quota_max_objects'] = \
-                self.parameters.get('Pool.quota_max_objects')
-            attrs['quota_max_bytes'] = \
-                self.parameters.get('Pool.quota_max_bytes')
+        attrs['name'] = self.parameters.get('Pool.poolname')
+        existing_name = NS._int.client.read(
+            "clusters/%s/Pools/%s/pool_name" % \
+            (
+                NS.tendrl_context.integration_id,
+                self.parameters['Pool.pool_id']
+            )
+        ).value
         Event(
             Message(
                 priority="info",
                 publisher=NS.publisher_id,
                 payload={
-                    "message": "Updating details for pool-id %s."
-                    " Attributes: %s" %
-                    (self.parameters['Pool.pool_id'],
-                     str(attrs))
+                    "message": "Renaming the pool:%s with new name: %s" %
+                    (existing_name,
+                     self.parameters.get('Pool.poolname'))
                     },
                 job_id=self.parameters['job_id'],
                 flow_id=self.parameters['flow_id'],
@@ -58,11 +45,12 @@ class Update(objects.BaseAtom):
         except RequestStateError as ex:
             Event(
                 Message(
-                    priority="info",
+                    priority="error",
                     publisher=NS.publisher_id,
                     payload={
-                        "message": "Failed to update pool %s."
-                        " Error: %s" % (self.parameters['Pool.pool_id'],
+                        "message": "Failed to rename pool: %s with new name: %s"
+                        " Error: %s" % (existing_name,
+                                        self.parameters.get('Pool.poolname'),
                                         ex)
                     },
                     job_id=self.parameters['job_id'],
@@ -77,8 +65,8 @@ class Update(objects.BaseAtom):
                 priority="info",
                 publisher=NS.publisher_id,
                 payload={
-                    "message": "Pool %s successfully updated" %
-                    (self.parameters['Pool.pool_id'])
+                    "message": "Pool: %s successfully renamed with new name: %s" %
+                    (existing_name, self.parameters.get('Pool.poolname'))
                     },
                 job_id=self.parameters['job_id'],
                 flow_id=self.parameters['flow_id'],
