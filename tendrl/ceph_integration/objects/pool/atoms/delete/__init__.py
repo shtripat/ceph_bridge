bb9fc312d9a6056fd782a666c3dc279bbb5aad42
Shubhendu <shtripat@redhat.com>
2017-05-19 01:04:02 +0530

    Made errors more prominent and removed the unwanted check
    While checking the update parameters for pool now we dont
    need to check the name and other parameters getting updated
    together as there is a separate rename flow available now.
    
    Signed-off-by: Shubhendu <shtripat@redhat.com>

--- tendrl/ceph_integration/objects/pool/atoms/delete/__init__.py
+++ tendrl/ceph_integration/objects/pool/atoms/delete/__init__.py
@@ -39,10 +39,10 @@ class Delete(objects.BaseAtom):
         except RequestStateError as ex:
             Event(
                 Message(
-                    priority="info",
+                    priority="error",
                     publisher=NS.publisher_id,
                     payload={
-                        "message": "Failed to delete pool %s."
+                        "message": "Failed to delete pool %s." \
                         " Error: %s" % (self.parameters['Pool.poolname'],
                                         ex)
                     },
