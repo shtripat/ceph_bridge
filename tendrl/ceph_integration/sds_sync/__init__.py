f1173c2f8581e8b0d919cc2cf8da73a5bd15c4fd
rishubhjain <rishubhjain47@gmail.com>
2017-06-21 19:13:07 +0530

    tendrl-bug-id: Tendrl/ceph-integration#306

--- tendrl/ceph_integration/sds_sync/__init__.py
+++ tendrl/ceph_integration/sds_sync/__init__.py
@@ -67,17 +67,20 @@ class CephIntegrationSdsSyncStateThread(sds_sync.SdsSyncThread):
     def _ping_cluster(self):
         NS.tendrl_context = NS.tendrl_context.load()
         NS.node_context = NS.node_context.load()
-        if NS.tendrl_context.cluster_id:
+        try:
+            if NS.tendrl_context.cluster_id:
                 cluster_data = ceph.heartbeat(NS.tendrl_context.cluster_id)
-                NS.tendrl_context.cluster_id = self.fsid = cluster_data['fsid']
-        else:
-            cluster_data = ceph.heartbeat()
-            if cluster_data:
+            else:
+                cluster_data = ceph.heartbeat()
+            if None not in cluster_data:
                 if "fsid" in cluster_data:
                     NS.tendrl_context.cluster_id = self.fsid = \
                         cluster_data['fsid']
-
-        NS.tendrl_context.cluster_name = self.name = cluster_data['name']
+                NS.tendrl_context.cluster_name = self.name = cluster_data['name']
+        except Exception as ex:
+            pass
+        if not hasattr(self,'name'):
+            self.name = None
         NS.tendrl_context.save()
 
     def _run(self):
